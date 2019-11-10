b953ec7864cc75e23bbe2177e2f4e362a933da64
heheda <zhangch99@outlook.com>
2019-11-10 10:23:08 +0800

    init ix

---
+++ src/ix/ix.h
@@ -0,0 +1,20 @@
+//
+// ix.h
+//
+//   Index Manager Component Interface
+//
+
+#ifndef IX_H
+#define IX_H
+
+// Please do not include any other files than the ones below in this file.
+
+#include "../redbase.h"  // Please don't change these lines
+#include "../rm/rm_rid.h"  // Please don't change these lines
+#include "../pf/pf.h"
+#include "ix_error.h"
+#include "ix_indexhandle.h"
+#include "ix_manager.h"
+#include "ix_indexscan.h"
+
+#endif // IX_H
